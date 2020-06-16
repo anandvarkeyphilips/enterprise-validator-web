e9bee8617fcf607e567e1c06478a8a55a0f08d00
Stalinbalraj Sowriappan <stalinbalraj@gmail.com>
2020-06-16 22:21:49 +0530

    Unit test cases and code coverage
    * adding test cases and resolving test case failures

--- src/app/error/error.component.spec.ts
+++ src/app/error/error.component.spec.ts
@@ -2,11 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorComponent } from './error.component';
 
+
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
-
+  const $ = require('jquery');
+  const window: string[] = [];
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ ErrorComponent ]
     })
@@ -21,5 +24,16 @@ describe('ErrorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should ngOnInit called', () => {
+    spyOn(component, 'letterHighlight').and.returnValue(true);
+    component.ngOnInit();
+    expect(component.letterHighlight).toHaveBeenCalled();
+  });
+  it('should letterHighlight called', () => {
+
+    spyOn($('.one'), 'addClass').and.callThrough();
+    const mockDom = '<li class="one">4</li>';
+    component.letterHighlight();
   });
 });
