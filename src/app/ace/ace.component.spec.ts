6411729a6a76fe6f7c90b1c60a86b5c07f6d6cdf
Stalinbalraj Sowriappan <stalinbalraj@gmail.com>
2020-06-16 17:04:59 +0530

    resolving test case failing

--- src/app/ace/ace.component.spec.ts
+++ src/app/ace/ace.component.spec.ts
@@ -2,15 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AceComponent } from './ace.component';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 describe('AceComponent', () => {
   let component: AceComponent;
   let fixture: ComponentFixture<AceComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        AceEditorModule
       ],
       declarations: [ AceComponent ]
     })
